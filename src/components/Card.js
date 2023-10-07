5e765eb5bd013e41aa73b797fcec0fee0e905f76
sserwangaedrisa <sserwangaedrisa@gmail.com>
2023-10-07 13:13:11 +0300

    Added more items in the robots list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const Card=({id, name, email})=>{
-
-    return(
-        <div className='tc bg-light-green ma2 dib br3 mr pa3 grow '>
-            <img alt='robot-image' src={`https://robohash.org/${id}`}/>
-            <div className=''>
-                <h1 className=''>{name}</h1>
-                <p>{email}</p>
-                </div>
-        </div>
-    )
-}
+const Card = ({ id, name, email }) => {
+  return (
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 export default Card;
--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import Card from './Card'
- 
-import {robots} from'./robots'
+import React from 'react';
+import Card from './Card';
+import './cardListStyle.css';
 
-const Cardlist=({robots})=>{
-    return (
-        <div>
-            {
-                robots.map((users, i)=>{
-                
-                    return (                        
-                            <Card 
-                            key={i}
-                            id={robots[i].id} 
-                            name={robots[i].name} 
-                            email={robots[i].email}
-                            />
-                        
-                        )
-                    }
-                )
-            }
-        </div>
-    )
-    
-}
+import { robots } from './robots';
+
+const Cardlist = ({ robots }) => {
+  return (
+    <div className='mainDiv'>
+      <div className='alignment '>
+        {robots.map((users, i) => {
+          return (
+            <Card
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Cardlist;
